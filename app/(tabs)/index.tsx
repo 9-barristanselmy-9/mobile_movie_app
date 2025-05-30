10f9cfcea9254aee5dc15d003e3c4e447eb5419d
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2025-05-30 16:26:23 +0100

    install appwrite

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -37,7 +37,6 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-
         {moviesLoading ? (
           <ActivityIndicator
             size="large"
